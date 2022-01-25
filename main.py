d3d8d5b712daa3b2842545b2aaa5543d8f62e49e
rkhusamov <khusamov@phystech.edu>
2022-01-26 00:24:08 +0300

    start bot dev

--- main.py
+++ main.py
@@ -3,15 +3,24 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+# Объявление бота
+import telebot
+bot = telebot.TeleBot("TOKEN", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+# TODO удалить токен перед заливкой онлайн
+
+
+tempp = 0
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}', tempp)  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/ изменения тест2 ветка test 2 changesgit
