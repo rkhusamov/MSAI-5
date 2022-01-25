10958f96496ac64e54f7a2bbc9dbfdc623463947
rkhusamov <khusamov@phystech.edu>
2022-01-26 00:46:08 +0300

    add bot autoreply

--- main.py
+++ main.py
@@ -5,11 +5,33 @@
 
 # Объявление бота
 import telebot
-bot = telebot.TeleBot("TOKEN", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+
+bot = telebot.TeleBot("ТОКЕН",parse_mode=None)
+# You can set parse_mode by default. HTML or MARKDOWN
+
+
 # TODO удалить токен перед заливкой онлайн
 
 
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Текст при старте")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
+
 tempp = 0
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -19,8 +41,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/ изменения тест2 ветка test 2 changesgit
